71ac60e8c256b14d986b3dce5f13476e0ffd5b74
Elid Venega <venegaelid@gmail.com>
2020-07-07 20:14:47 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+let d, h, m, s, animate;
+
+function time() {
+    d = new Date();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+    clock();
+};
+
+function clock() {
+    s++;
+    if (s == 60) {
+        s = 0;
+        m++;
+        if (m == 60) {
+            m = 0;
+            h++;
+            if (h == 24) {
+                h = 0;
+            }
+        }
+    }
+    $('sec', s);
+    $('min', m);
+    $('hr', h);
+    animate = setTimeout(clock, 1000);
+};
+
+function $(id, val) {
+    if (val < 10) {
+        val = '0' + val;
+    }
+    document.getElementById(id).innerHTML = val;
+};
+
+window.onload = time;
